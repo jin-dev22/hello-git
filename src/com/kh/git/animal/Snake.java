fcaa4262e25ffae431a78785771e6820ed67dbc0
jin_dev22 <59607064+jin-dev22@users.noreply.github.com>
2022-06-13 20:36:45 +0900

    Snake#bites 메소드 추가

--- src/com/kh/git/animal/Snake.java
+++ src/com/kh/git/animal/Snake.java
@@ -3,4 +3,9 @@ package com.kh.git.animal;
 public class Snake {
   public void run() {
     System.out.println("Snake run..");
+  }
+  
+  public void bite(){
+    System.out.println("Snake bites..");
+  }
 }
