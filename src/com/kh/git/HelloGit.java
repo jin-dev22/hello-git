748cb339080d2cb535848540db36636fc3d4220a
jin-dev22 <jinpark.dev22@gmail.com>
2022-06-13 16:58:21 +0900

    HelloGit클래스에 메소드 추가. 메인메소드에서 객체생성
    더미메소드 만들고 메인메소드에서 객체생성해서 호출하기

--- src/com/kh/git/HelloGit.java
+++ src/com/kh/git/HelloGit.java
@@ -3,6 +3,11 @@ package com.kh.git;
 public class HelloGit {
 	public static void main(String[] args) {
 		System.out.println("HelloGit!!!");
+		HelloGit hg1 = new HelloGit();
+		hg1.dummy();
 	}
 	
+	public void dummy() {
+		System.out.println("더어어어어어어어어어어어어어어어미");
+	}
 }
